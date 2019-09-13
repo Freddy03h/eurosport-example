c73276f8d2ec97e044ce1c45f4b787d56b74ed34
freddy <freddy03h@gmail.com>
2019-09-13 11:28:29 +0200

    load and display players

--- src/PlayersList.tsx
+++ src/PlayersList.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 
-const PlayersList = () => {
+function _keyExtractor(item) {
+  return item.shortname; // assuming it's the id…
+}
+
+const renderPlayerRow = ({item: player}) => {
   return (
     <View>
-      <Text>PlayersList</Text>
+      <Text>{player.shortname}</Text>
     </View>
   );
 };
+
+const PlayersList = () => {
+  const [players, setPlayers] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://eurosportdigital.github.io/eurosport-react-native-developer-recruitment/docs/headtohead.json',
+    )
+      .then(response => response.json())
+      .then(json => setPlayers(json.players));
+  }, []);
+
+  if (!players) {
+    return (
+      <View style={styles.loadingView}>
+        <ActivityIndicator animating={true} size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={players}
+      style={styles.container}
+      keyExtractor={_keyExtractor}
+      renderItem={renderPlayerRow}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  loadingView: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flexGrow: 1,
+  },
+});
+
+PlayersList.navigationOptions = () => ({
+  headerTitle: 'Liste des joueurs',
+});
 
 export default PlayersList;
